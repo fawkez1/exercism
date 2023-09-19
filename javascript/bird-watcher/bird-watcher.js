ce0733aa40b13022d29678d5e464c8b20de1f96f
fawkez1 <adrian_massaro@gmx.net>
2023-09-19 21:41:52 +0200

    finished the birdwatcher task. harder one.

--- javascript/bird-watcher/bird-watcher.js
+++ javascript/bird-watcher/bird-watcher.js
@@ -28,7 +28,19 @@ export function totalBirdCount(birdsPerDay) {
  * @returns {number} birds counted in the given week
  */
 export function birdsInWeek(birdsPerDay, week) {
- 
+ // loop erschaffen, der bei:
+    // eingabe "1": objekt auf position 0 bis 6 auswaehlt:
+        // array start : let start = ('2' = [7*'2']-1) also 13. 
+        // array ende : start + 7
+        // ergebnis return (13, 20) an neue variable (let arrayPositions)
+        // 
+// inhalt von dokument birdsValue summieren = result
+let result = 0;
+for (let i = (week * 7 - 7); i < (week * 7 ); i++){
+  if (typeof birdsPerDay[i] === 'number')
+  result += birdsPerDay [i];
+}
+return result;
 }
 
 /**
@@ -39,5 +51,14 @@ export function birdsInWeek(birdsPerDay, week) {
  * @returns {number[]} corrected bird count data
  */
 export function fixBirdCountLog(birdsPerDay) {
-  throw new Error('Please implement the fixBirdCountLog function');
+  const result = [];
+  for (let i = 0; i < birdsPerDay.length; i++){
+    
+    if (i % 2 === 0){
+      birdsPerDay[i] = birdsPerDay[i] + 1;
+    }
+      
+    }
+    return birdsPerDay;
 }
+
