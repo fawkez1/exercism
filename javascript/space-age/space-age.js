a9e21305adbcc0df63daac014f8fb7b0fa47e2a5
fawkez1 <adrian_massaro@gmx.net>
2023-09-28 22:36:40 +0200

    finished all planets

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -8,32 +8,34 @@ import { parse } from "path";
 export const age = (planet, age) => {
   const ageInYears = age / 31557600;
   const planets = {
-    Mercury : 0.2408467,
-    Venus : 0.61519726,
-    Earth : 1,
-    Mars : 1.8808158,
-    Jupiter : 1.862615,
-    Saturn : 29.447498,
-    Uranus : 84.016846,
-    Neptune : 164.79132,
+    mercury : 0.2408467,
+    venus : 0.61519726,
+    earth : 1,
+    mars : 1.8808158,
+    jupiter : 11.862615,
+    saturn : 29.447498,
+    uranus : 84.016846,
+    neptune : 164.79132,
   }
   switch(planet){
     case 'earth' :
       return parseFloat(ageInYears.toFixed(2))
     case 'mercury' :
-      return parseFloat((ageInYears * planets[Mercury]).toFixed(2))
+      return parseFloat((ageInYears / planets['mercury']).toFixed(2));
     case 'venus' :
-      return ageInYears * planets[Venus];
+      return parseFloat((ageInYears / planets['venus']).toFixed(2));
     case 'mars' :
-      return ageInYears * planets[Mars];
+      return parseFloat((ageInYears / planets['mars']).toFixed(2));
     case 'jupiter' :
-      return ageInYears * planets[Jupiter];
+      return parseFloat((ageInYears / planets['jupiter']).toFixed(2));
     case 'saturn' :
-      return ageInYears * planets[Saturn];
+      return parseFloat((ageInYears / planets['saturn']).toFixed(2));
     case 'uranus' :
-      return ageInYears * planets[Uranus];
+      return parseFloat((ageInYears / planets['uranus']).toFixed(2));
     case 'neptune' :
-      return ageInYears * planets[Neptune];
+      return parseFloat((ageInYears / planets['neptune']).toFixed(2));
+
+
     default :
     return "NaP - Not a Planet"
 
