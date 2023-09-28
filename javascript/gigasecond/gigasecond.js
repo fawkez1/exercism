54a7df832b7d5198131d9528ad8cc47186475599
fawkez1 <adrian_massaro@gmx.net>
2023-09-28 09:27:53 +0200

    finished the gigasecond

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,6 +3,17 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
+export const gigasecond = (date) => {
+  const gigasecond = 1e9; 
+  const milliseconds = date.getTime();
+  return new Date(milliseconds + gigasecond * 1000);
+
+
+  // datum format = (2023, 8, 27)
+  // (2023 * year) + (8 * month) + (27 * day)
+  // year = 31536000000 ms
+  // month = 2628000000 ms
+  // day = 86400000 ms
+  // gigasecond = 1000000000 ms
+  // 
 };
