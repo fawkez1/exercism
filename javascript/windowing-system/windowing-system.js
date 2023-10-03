c9b2ca3c77998aa7029c4e8ab261cc0c845582e2
fawkez1 <adrian_massaro@gmx.net>
2023-10-03 13:26:58 +0200

    finished the first 4 tasks

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -6,64 +6,81 @@
  * you defined so they are available for the tests.
  */
 
-export function Size (width, height){
-    if (width){
-        this.width = width;
-    }
-    else this.width = 80;
 
-    if (height){
+export class Size {
+    constructor(width = 80, height = 60) {
+        this.width = width;
         this.height = height;
     }
-    else this.height = 60;
-    
-}
-Size.prototype.resize = function(newWidth, newHeight){
-    this.width = newWidth;
-    this.height = newHeight;
-}
 
-export function Position (x, y){
-    if(x){
-        this.x = x;
+    resize(newWidth, newHeight) {
+        this.width = newWidth;
+        this.height = newHeight;
     }
-    else this.x = 0;
+}
 
-    if(y){
+
+// Definition der Position-Klasse
+export class Position {
+    constructor(x = 0, y = 0) {
+        this.x = x;
         this.y = y;
     }
-    else this.y = 0;
-}
 
-Position.prototype.move = function (newX, newY){
-    this.x = newX;
-    this.y = newY;
-}
-
-export function ProgrammWindow(screenSize, size, position){
-    this.screenSize = screenSize
-    this.size = size
-    this.position = position
-
-    if (screenSize){
-        this.screenSize = screenSize
+    move(newX, newY) {
+        this.x = newX;
+        this.y = newY;
     }
-    else this.screenSize = 800, 600;
-    
-    if(size){
-        this.size = size;
-    }
-    else this.size = Size;
-
-    if(position){
-        this.position = position;
-    }
-    else this.position = Position;
 }
-ProgrammWindow.prototype.newProgrammWindow = function (newScreensize, newSize, newPosition){
-    this.screenSize = newScreensize;
-    this.size = newSize;
-    this.position = newPosition;
 
+// Definition der ProgrammWindow-Klasse
+export class ProgramWindow {
+    constructor() {
+        this.screenSize = new Size(800, 600);
+        this.size = new Size(); // Erstellen einer Instanz der Size-Klasse
+        this.position = new Position(); // Erstellen einer Instanz der Position-Klasse
+    }
+    resize (newSize){
+        if (newSize.height < 1){
+            newSize.height = 1;
+        }
+        else if (newSize.height > this.screenSize.height){
+            newSize.height = this.screenSize.height;
+        }
+
+        if (newSize.width < 1){
+            newSize.width = 1;
+        }
+        else if(newSize.width > this.screenSize.width){
+            newSize.width = this.screenSize.width;
+        }
+        this.size = newSize;
 }
+
+
+    move(newPosition){
+        // x-Position
+        if(newPosition.x < 0){
+           newPosition.x = 0;
+        }
+        
+        // y-Position
+        if(newPosition.y < 0){
+            newPosition.y = 0;
+        }
+        
+        // final 
+        this.position = newPosition;
+    }
+}
+// Erstellung einer Instanz der ProgramWindow-Klasse
+export const newProgramWindow = new ProgramWindow();
+
+
+
+
+
+
+
+
 
