daa0811f245b7e11c386090fd8d0df9155a59269
fawkez1 <adrian_massaro@gmx.net>
2023-10-03 22:26:18 +0200

    4passed 3failed with greedy algorithm

--- javascript/knapsack/knapsack.js
+++ javascript/knapsack/knapsack.js
@@ -1,8 +1,28 @@
-//
-// This is only a SKELETON file for the 'Knapsack' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
 export const knapsack = (maximumWeight, items) => {
-  throw new Error('Remove this statement and implement this function');
+  // Erstellt ein Array zur Verfolgung der ausgewählten Gegenstände
+  const selectedItems = [];
+
+  // Sortiert die Gegenstände nach dem Verhältnis von Wert zu Gewicht (Value-to-Weight Ratio)
+  items.sort((a, b) => b.value / b.weight - a.value / a.weight);
+
+  // Initialisiert das aktuelle Gewicht und den Gesamtwert auf 0
+  let currentWeight = 0;
+  let totalValue = 0;
+
+  // Durchlaufe die sortierten Gegenstände
+  for (const item of items) {
+    // Überprüfe, ob der Gegenstand noch in den Rucksack passt
+    if (currentWeight + item.weight <= maximumWeight) {
+      if (!selectedItems.includes(item)){
+      // Füge den Gegenstand dem Rucksack hinzu
+      selectedItems.push(item);
+      currentWeight += item.weight;
+      totalValue += item.value;
+      }
+    }
+  }
+  // Rückgabe des Gesamtwerts
+  return totalValue;
+  
 };
