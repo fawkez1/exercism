88a815c337712c2e1d0cde197bc4640b60f4874a
fawkez1 <adrian_massaro@gmx.net>
2023-10-06 13:27:01 +0200

    finished dnd. constuctors are important

--- javascript/dnd-character/dnd-character.js
+++ javascript/dnd-character/dnd-character.js
@@ -1,53 +1,64 @@
-//
-// This is only a SKELETON file for the 'D&D Character' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-let rolls = [];
 
-// roll dice 4x and safe in 'rolls'
-function rollDice(){
-for (let i = 1; i < 4; i++){
-  rolls =+ Math.random();
-}
-return rolls;
-}
-
-
-export const abilityModifier = () => {
-  rollDice();
-  rolls.sort
+export const abilityModifier = (score) => {
+  if (score < 3){
+    throw new Error ('Ability scores must be at least 3');
+  }
+  else if (score > 18){
+    throw new Error('Ability scores can be at most 18');
+  }
+  let modifier = Math.floor((score - 10) / 2);
+  return modifier;
 };
 
 export class Character {
+  constructor() {
+    // Generate ability scores during character creation
+    // use the constructor so store results and use them later on.
+    this.strengthValue = Character.rollAbility();
+    this.dexterityValue = Character.rollAbility();
+    this.constitutionValue = Character.rollAbility();
+    this.intelligenceValue = Character.rollAbility();
+    this.wisdomValue = Character.rollAbility();
+    this.charismaValue = Character.rollAbility();
+  }
+// rolls must be equal to dice sides!!!!!!
   static rollAbility() {
-    throw new Error('Remove this statement and implement this function');
+    const rolls = [];
+    for (let i = 0; i < 4; i++) {
+      rolls.push(Math.floor(Math.random() * 6) + 1);
+    }
+    rolls.sort((a, b) => a - b); // sort in ascending order
+    rolls.shift();
+    const sum = rolls.reduce((acc, value) => acc + value, 0);
+    return sum;
   }
 
   get strength() {
-    throw new Error('Remove this statement and implement this function');
+    return this.strengthValue;
   }
 
   get dexterity() {
-    throw new Error('Remove this statement and implement this function');
+    return this.dexterityValue;
   }
 
   get constitution() {
-    throw new Error('Remove this statement and implement this function');
+    return this.constitutionValue;
   }
 
   get intelligence() {
-    throw new Error('Remove this statement and implement this function');
+    return this.intelligenceValue;
   }
 
   get wisdom() {
-    throw new Error('Remove this statement and implement this function');
+    return this.wisdomValue;
   }
 
   get charisma() {
-    throw new Error('Remove this statement and implement this function');
+    return this.charismaValue;
   }
 
   get hitpoints() {
-    throw new Error('Remove this statement and implement this function');
+    const sum = 10 + abilityModifier(this.constitutionValue);
+    return sum;
   }
 }
