dad47ea3e319250a38eb5e9bd46b0746b0d5bb01
fawkez1 <adrian_massaro@gmx.net>
2023-10-10 15:21:03 +0200

    added a user code, since i was not able to solve the exercise on my own.

--- javascript/transpose/transpose.js
+++ javascript/transpose/transpose.js
@@ -1,29 +1,14 @@
-//
-// This is only a SKELETON file for the 'Transpose' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+console.log('Hello, World!');
 
-export const transpose = (text) => {
-  let fullString = [];
-  const row = text.length;
-  const column = text[0].length;
-  let result = [];
-  for (let i = 0; i < text.length - 1; i ++) {
-    fullString.push(text[i] + text[i + 1]);
+export const transpose = (arr) => {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    const longest = Math.max(...arr.slice(i).map((element) => element.length));
+    for (let j = 0; j < longest; j++) {
+      const count = result[j] || '';
+      result[j] = count + (arr[i][j] ? arr[i][j] : ' ');
+    }
   }
-  for (let i = 0; i < row; i++){
-  result = fullString[i + 0] + fullString[i + row]
-  }
+
   return result;
 };
-
-
-// zeilen: ganze woerter sind zeilen
-// spalte: der anfang eines wortes ist eine spalte
-
-// input = ['ABC', '123']                 "ABC"
-//                                        "123"
-
-// output = ['A1', 'B2', 'C3']            "A1"
-//                                        "B2"
-//                                        "C3"
