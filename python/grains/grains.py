accdf96cedcd5f2bd75405ca4206968b63ef65da
fawkez1 <adrian_massaro@gmx.net>
2024-08-25 13:04:09 +0200

    pyhton snytax is so much more natural

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,20 @@
 def square(number):
-    return number
+    if number > 0 and number <= 64:
+        return 2**(number-1)
+    else:
+        raise ValueError ("square must be between 1 and 64")
+
+"""
+Feld 1 = 1
+Feld 2 = 2
+Feld 3 = 4
+Feld 5 = 8
+Feld 6 = 16
+"""
 
 
 def total():
-    pass
+    total_grains = 0
+    for square in range(1,65):
+        total_grains += 2**(square -1)
+    return total_grains
